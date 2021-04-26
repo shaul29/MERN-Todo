067b310f77936762d102aec6e7e452c2e75f175d
Daniel Shaul <daniel.shaul8@gmail.com>
2021-04-26 19:04:48 -0400

    preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import colors from 'colors'
 import connectDB from './config/db.js'
@@ -15,12 +16,17 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
 
 app.use('/api/users', userRoutes)
 app.use('/api/todo', todoRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
