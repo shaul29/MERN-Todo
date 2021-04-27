c1c6414ddbae3c6b176c04209f1b708af5c0c4b1
Daniel Shaul <daniel.shaul8@gmail.com>
2021-04-27 12:08:54 -0400

    heroku deployment attempt 3

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,14 @@ app.use('/api/todo', todoRoutes)
 const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join('frontend/build')))
+    app.use(express.static(path.join(__dirname, '..', '/frontend/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....')
     })
 }
 
